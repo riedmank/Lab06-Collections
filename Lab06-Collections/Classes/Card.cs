a4589a5bdbd7fc7b7710a6495b2e7a09f57f7b13
Kristofer Riedman <riedmank@hotmail.com>
2018-10-17 08:47:01 -0700

    created card objects

---
+++ Lab06-Collections/Classes/Card.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab06_Collections.Classes
+{
+    class Card
+    {
+        public Suit Suit { get; set; }
+        public Value Value { get; set; }
+
+        public Card(Suit suit, Value value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+    }
+
+    enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    enum Value
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
--- Lab06-Collections/Program.cs
+++ Lab06-Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Lab06_Collections.Classes;
 
 namespace Lab06_Collections
 {
@@ -7,6 +8,37 @@ namespace Lab06_Collections
         static void Main(string[] args)
         {
             
+        }
+
+        static void DeckOfCards()
+        {
+            Card card1 = new Card(Suit.Hearts, Value.Ace);
+            Card card2 = new Card(Suit.Diamonds, Value.King);
+            Card card3 = new Card(Suit.Spades, Value.Seven);
+            Card card4 = new Card(Suit.Clubs, Value.Ten);
+            Card card5 = new Card(Suit.Hearts, Value.Two);
+            Card card6 = new Card(Suit.Diamonds, Value.Five);
+            Card card7 = new Card(Suit.Spades, Value.Six);
+            Card card8 = new Card(Suit.Clubs, Value.Queen);
+            Card card9 = new Card(Suit.Hearts, Value.Three);
+            Card card10 = new Card(Suit.Diamonds, Value.Eight);
+            Card card11 = new Card(Suit.Spades, Value.Four);
+            Card card12 = new Card(Suit.Clubs, Value.Jack);
+
+            Deck<Card> MyDeck = new Deck<Card>();
+
+            MyDeck.Add(card1);
+            MyDeck.Add(card2);
+            MyDeck.Add(card3);
+            MyDeck.Add(card4);
+            MyDeck.Add(card5);
+            MyDeck.Add(card6);
+            MyDeck.Add(card7);
+            MyDeck.Add(card8);
+            MyDeck.Add(card9);
+            MyDeck.Add(card10);
+            MyDeck.Add(card11);
+            MyDeck.Add(card12);
         }
     }
 }
